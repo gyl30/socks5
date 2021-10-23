5a5257ded7faf64a94e43bd1911eb649703a37ef
gyl <im.ggyyll@gmail.com>
2021-10-23 11:52:32 +0800

    coroutine

--- local.cpp
+++ local.cpp
@@ -1,13 +1,30 @@
-#include <thread>
-#include <memory>
+#include <cstdlib>
 #include <functional>
-#include <boost/asio.hpp>
-#include <boost/asio/spawn.hpp>
-#include <boost/asio/signal_set.hpp>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+
+#include <asio.hpp>
+#include <asio/awaitable.hpp>
+#include <asio/co_spawn.hpp>
+#include <asio/detached.hpp>
+#include <asio/io_context.hpp>
+#include <asio/ip/tcp.hpp>
+#include <asio/read_until.hpp>
+#include <asio/redirect_error.hpp>
+#include <asio/signal_set.hpp>
+#include <asio/steady_timer.hpp>
+#include <asio/strand.hpp>
+#include <asio/use_awaitable.hpp>
+#include <asio/write.hpp>
+#include <stdint.h>
+
 #include "log.h"
-static std::string socket_address(const boost::asio::ip::tcp::socket& socket)
+
+static std::string socket_address(const asio::ip::tcp::socket& socket)
 {
-    boost::system::error_code ec;
+    std::error_code ec;
     auto ed = socket.remote_endpoint(ec);
     if (ec)
     {
@@ -27,140 +44,111 @@ static std::string socket_address(const boost::asio::ip::tcp::socket& socket)
 class session : public std::enable_shared_from_this<session>
 {
    public:
-    session(boost::asio::ip::tcp::socket socket)
-        : local_socket_(std::move(socket)), remote_socket_(socket.get_executor()), strand_(socket.get_executor())
+    session(asio::ip::tcp::socket socket) : socket_(std::move(socket)), remote_socket_(socket_.get_executor()) {}
+    ~session() { LOG_DEBUG << "session finish"; }
+
+    void go()
     {
+        auto r = [self = shared_from_this()] { return self->do_handshake(); };
+        asio::co_spawn(socket_.get_executor(), r, asio::detached);
     }
-    ~session() {}
-
-    void go() { handshake(); }
-
-   private:
-    void handshake()
+    asio::awaitable<void> do_handshake()
     {
-        boost::asio::spawn(strand_, [this, self = shared_from_this()](auto yield) { do_handshake(yield); });
-    }
-
-    void forward()
-    {
-        // clang-format off
-        auto local_to_remote = [this, self = shared_from_this()](auto yield) { do_forward(local_socket_, " --> ", remote_socket_, yield); };
-        auto remote_to_local = [this, self = shared_from_this()](auto yield) { do_forward(remote_socket_, " <-- ", local_socket_, yield); };
-        boost::asio::spawn(strand_, local_to_remote);
-        boost::asio::spawn(strand_, remote_to_local);
-        // clang-format on
-    }
-
-    void do_handshake(boost::asio::yield_context yield)
-    {
-        // connect remote
-        boost::system::error_code ec;
-        boost::asio::ip::tcp::resolver resolver(local_socket_.get_executor());
+        std::error_code ec;
+        auto err = asio::redirect_error(asio::use_awaitable, ec);
+        asio::ip::tcp::resolver resolver(socket_.get_executor());
         std::string addr = "127.0.0.1";
-        uint16_t port = 5555;
-        std::string host = "127.0.0.1:5555";
-        auto const it = resolver.async_resolve(addr, std::to_string(port), yield[ec]);
+        uint16_t port = 1080;
+        std::string host = "127.0.0.1:1080";
+        auto const it = co_await resolver.async_resolve(addr, std::to_string(port), err);
         if (ec)
         {
             LOG_ERROR << "resulve remote " << host << " failed --> " << ec.message();
-            return;
+            co_return;
         }
-        boost::asio::async_connect(remote_socket_, it.begin(), it.end(), yield[ec]);
+        co_await asio::async_connect(remote_socket_, it.begin(), it.end(), err);
         if (ec)
         {
             LOG_ERROR << "connect remote " << host << " failed --> " << ec.category().name() << ":" << ec.message();
-            return;
+            co_return;
         }
-
-        LOG_DEBUG << "local " << host << " connect remote " << socket_address(remote_socket_) << " successful";
+        LOG_DEBUG << "connect remote " << host << " successful";
         forward();
     }
-    void do_forward(boost::asio::ip::tcp::socket& src, const std::string& dot, boost::asio::ip::tcp::socket& dst,
-                    boost::asio::yield_context yield)
+    void forward()
+    {
+        auto l = [this, self = shared_from_this()] { return do_forward(socket_, remote_socket_); };
+        auto r = [this, self = shared_from_this()] { return do_forward(remote_socket_, socket_); };
+        asio::co_spawn(socket_.get_executor(), l, asio::detached);
+        asio::co_spawn(socket_.get_executor(), r, asio::detached);
+    }
+
+    asio::awaitable<void> do_forward(asio::ip::tcp::socket& src, asio::ip::tcp::socket& dst)
     {
         std::string src_address = socket_address(src);
         std::string dst_address = socket_address(dst);
-        boost::system::error_code ec;
+        std::error_code ec;
+        auto err = asio::redirect_error(asio::use_awaitable, ec);
+
         for (;;)
         {
             char data[32 * 1024] = {0};
-            std::size_t n = src.async_read_some(boost::asio::buffer(data), yield[ec]);
+            std::size_t n = co_await src.async_read_some(asio::buffer(data), err);
             if (ec)
             {
-                LOG_ERROR << "local read failed " << ec.message();
+                LOG_ERROR << "read from " << src_address << " failed " << ec.message();
                 break;
             }
-            LOG_DEBUG << src_address << dot << dst_address;
-            boost::asio::async_write(dst, boost::asio::buffer(data, n), yield[ec]);
+            co_await asio::async_write(dst, asio::buffer(data, n), err);
             if (ec)
             {
-                LOG_ERROR << "remote write failed " << ec.message();
+                LOG_ERROR << "write to " << dst_address << " failed " << ec.message();
                 break;
             }
         }
-        LOG_DEBUG << src_address << dot << dst_address << " finish";
-        src.close(ec);
-        dst.close(ec);
+        LOG_DEBUG << src_address << " to " << dst_address << " finish";
+        close();
+    }
+
+    void close()
+    {
+        std::error_code ignore;
+        socket_.close(ignore);
+        remote_socket_.close(ignore);
     }
 
    private:
-    boost::asio::ip::tcp::socket local_socket_;
-    boost::asio::ip::tcp::socket remote_socket_;
-    boost::asio::strand<boost::asio::any_io_executor> strand_;
+    asio::ip::tcp::socket socket_;
+    asio::ip::tcp::socket remote_socket_;
 };
+asio::awaitable<void> listener(asio::ip::tcp::acceptor acceptor)
+{
+    std::error_code ec;
+    auto err = asio::redirect_error(asio::use_awaitable, ec);
+    for (;;)
+    {
+        auto socket = co_await acceptor.async_accept(err);
+        if (ec)
+        {
+            LOG_ERROR << "accept failed " << ec.message();
+        }
+        else
+        {
+            std::make_shared<session>(std::move(socket))->go();
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    uint16_t port = 6666;
+    asio::io_context io_context(1);
 
-    boost::asio::io_context io(1);
-    boost::asio::io_context so(1);
+    unsigned short port = 5555;
+    co_spawn(io_context, listener(asio::ip::tcp::acceptor(io_context, {asio::ip::tcp::v4(), port})), asio::detached);
+    asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    signals.async_wait([&](auto, auto) { io_context.stop(); });
 
-    boost::asio::ip::tcp::endpoint ed(boost::asio::ip::tcp::v4(), port);
-
-    auto fn = [&](boost::asio::yield_context yield)
-    {
-        boost::asio::ip::tcp::acceptor acceptor(io, ed);
-
-        LOG_DEBUG << "run acceptor " << ed.address().to_string();
-
-        for (;;)
-        {
-            boost::system::error_code ec;
-            boost::asio::ip::tcp::socket socket(so);
-            acceptor.async_accept(socket, yield[ec]);
-            if (ec)
-            {
-                LOG_ERROR << "acceptor error --> " << ec.category().name() << ":" << ec.message();
-            }
-            else
-            {
-                LOG_DEBUG << "accept socket --> " << socket_address(socket);
-                socket.set_option(boost::asio::ip::tcp::no_delay(true));
-                std::make_shared<session>(std::move(socket))->go();
-            }
-        }
-    };
-
-    boost::asio::spawn(io, fn);
-
-    boost::asio::signal_set signals(io, SIGINT, SIGTERM);
-    signals.async_wait(
-        [&](auto, auto)
-        {
-            so.stop();
-            io.stop();
-        });
-
-    std::thread(
-        [&so]
-        {
-            auto w = boost::asio::make_work_guard(so);
-            so.run();
-        })
-        .detach();
-
-    io.run();
+    io_context.run();
 
     return 0;
 }
